8061fb8582fd8493f4a0d120437ce0666e3ec164
Kevin Ramirez <kevinramirez266@yahoo.com>
2024-03-03 13:56:46 -0800

    Movement script edits
    Movement script under work

--- RCAndFriendsGame/Assets/Scripts/PlayerMovement.cs
+++ RCAndFriendsGame/Assets/Scripts/PlayerMovement.cs
@@ -12,10 +12,10 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //rigidBody = GetComponent<Rigidbody>();
-        playerStats = new StatSheet();
-        playerStats.Health = 20f;
-        playerStats.Speed = 1f;
+        rigidBody = GetComponent<Rigidbody>();
+        playerStats = new StatSheet(20f, 2f);
+        //playerStats.Health = 20f;
+        //playerStats.Speed = 2f;
     }
 
     // Update is called once per frame
@@ -23,11 +23,20 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontalMovement = Input.GetAxis("Horizontal");
         float verticalMovement = Input.GetAxis("Vertical");
-        Console.WriteLine(horizontalMovement + " " + verticalMovement);
-
-        if (horizontalMovement > 0)
+        Vector3 moveVector = Vector3.zero;
+        if (horizontalMovement >= 0 && verticalMovement >= 0)
         {
-            
+            moveVector = new Vector3(horizontalMovement, 0f, 0f);
         }
+        else if (horizontalMovement != 0) 
+        {
+            moveVector = new Vector3(horizontalMovement, 0f, -horizontalMovement);
+        } 
+        else if (verticalMovement != 0)
+        {
+            moveVector = new Vector3(verticalMovement, 0f, verticalMovement);
+        }
+
+        rigidBody.velocity = Vector3.Normalize(moveVector) * playerStats.Speed;
     }
 }
--- RCAndFriendsGame/Assets/Scripts/StatSheet.cs
+++ RCAndFriendsGame/Assets/Scripts/StatSheet.cs
@@ -7,6 +7,18 @@ public class StatSheet
     private float _health;
     private float _speed;
     
+    public StatSheet() 
+    {
+        _health = 0;
+        _speed = 0;
+    }
+
+    public StatSheet(float health, float speed)
+    {
+        _health=health;
+        _speed=speed;
+    }
+
     public float Health
     {
         get { return _health; }
